d4390352b6c49cbce4700d06f3ec2bbd79b8283f
ksrikanth-dev <ksrikanth333@gmail.com>
2021-03-26 18:44:11 +0530

    operators and few others basic stuff

--- Day2/c_keywords.cpp
+++
@@ -1,7 +0,0 @@
-/* 
-Keywords that are common for C and C++:
-***************************************
-auto double break else case enum char extern const float continue for default goto do if
-int long register return short signed sizeof static
-struct switch typedef union unsigned void volatile while
-*/
--- Day2/operators1.cpp
+++ Day2/operators1.cpp
@@ -19,17 +19,29 @@ int main() {
     int j;
     i = 1;
     /* left shift */
-    for(j=0; j<4; j++) {
+    for(j=1; j<5; j++) {
        i = i << 1;  /* left shift i by 1, which
                        is same as a multiply by 2 */
        cout<<"Left shift "<<j<<" : "<<i<<endl;
      }
      /* right shift */
-     for(j=0; j<4; j++) {
+     for(j=1; j<5; j++) {
        i = i >> 1;  /* right shift i by 1, which
                        is same as a division by 2 */
        cout<<"Right shift "<<j<<" : "<<i<<endl;
      }
-     
+
+     string msg = "Hello world";
+     cout<<endl<<"msg: "<<msg<<endl;
+     string encoded_msg, decoded_msg;
+     for(auto ch : msg) {
+       encoded_msg += encode(ch);
+     }
+     cout<<"encoded message: "<<encoded_msg<<endl;
+     for(auto ch : encoded_msg) {
+       decoded_msg += decode(ch);
+     }
+     cout<<"decoded message: "<<decoded_msg<<endl;
+
     return 0;
 }
